18f59801310427dedac96e3d958858554ea38ff6
DimaProhorenko <dima.study.web@gmail.com>
2023-04-08 07:50:50 +0300

    backdrop and mobile nav functionality

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,19 +1,43 @@
 const burger = document.querySelector('.burger');
+const closeMNBtn = document.querySelector('.mn__close-btn');
+const mobileMenu = document.querySelector('.mn');
+
+const backdrop = document.querySelector('.backdrop');
+
 const mobileSearchFormOpen = document.querySelector('.open-search');
 const mobileSearchFormClose = document.querySelector('.close-search');
 const mobileSearchForm = document.querySelector('.header__mobile-form');
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('opened');
-    // burger.setAttribute('aria-expanded', );
+const showBackdrop = () => {
+    backdrop.classList.add('active');
+}
+const hideBackdrop = () => {
+    backdrop.classList.remove('active');
+}
 
-    if (burger.ariaExpanded == 'true') {
-        burger.setAttribute('aria-expanded', false);
-    } else {
-        burger.setAttribute('aria-expanded', true);
-    }
-    
+const backdropClickHandler = () => {
+    hideBackdrop();
+    hideMobileMenu();
+}
+
+const hideMobileMenu = () => {
+    mobileMenu.classList.remove('opened');
+}
+
+burger.addEventListener('click', () => {
+    burger.classList.add('opened');
+    burger.setAttribute('aria-expanded', true);
+    mobileMenu.classList.add('opened');
+    showBackdrop();
 })
+
+closeMNBtn.addEventListener('click', () => {
+    hideMobileMenu();
+    hideBackdrop();
+})
+
+backdrop.addEventListener('click', backdropClickHandler)
+
 
 mobileSearchFormOpen.addEventListener('click', () => {
     mobileSearchForm.classList.add('opened');
